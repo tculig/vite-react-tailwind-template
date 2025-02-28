6f9769b1629545b145e7633528c51500c6090265
Tihomir Culig <tihomirculig@gmail.com>
2025-02-28 23:05:22 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p>Start prompting (or editing) to see magic happen :)</p>
+      <p>Empty page</p>
     </div>
   )
 }
